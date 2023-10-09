25f0e52302c72ab442e850b66f4a3e6210470797
Mkarimifar_utsMacbook <em.karimifar@gmail.com>
2023-10-08 22:16:00 -0500

    first commit

---
+++ gatsby-starter-default/src/components/TestComp.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const TestComp = ({ text }) => {
+  return (
+    <div>
+      <h1>{text} </h1> <h2>Component test</h2>
+    </div>
+  )
+}
+
+export default TestComp
---
+++ gatsby-starter-default/src/components/header.js
@@ -0,0 +1,67 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+//styles
+import "../styles/global.css"
+import * as styles from "./header.module.css"
+
+//scripts
+import "../scripts/app.js"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons"
+
+const Header = ({ siteTitle }) => {
+  // const location = useLocation()
+  // console.log(location)
+  return (
+    <header>
+      <Link id="site-title" to="/">
+        <img
+          alt="Em Karimifar Logo"
+          draggable="false"
+          src="./assets/logo-bg.svg"
+        />
+        <h1>
+          Em <br />
+          Karimifar
+        </h1>
+      </Link>
+
+      <nav>
+        <ul className="nav-items">
+          <li>
+            <Link to="/" activeClassName="current-page" partiallyActive={true}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/works"
+              activeClassName="current-page"
+              partiallyActive={true}
+            >
+              Works
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              activeClassName="current-page"
+              partiallyActive={true}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="#">
+              <FontAwesomeIcon icon={faCircleHalfStroke} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
